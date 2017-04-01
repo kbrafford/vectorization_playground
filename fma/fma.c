c49087b61aaa0eafdaa746b0b3dd64ee39fd111e
Keith Brafford <keith.brafford@gmail.com>
2017-04-01 01:16:10 -0700

    added AVX2 Horner's method PRE-TEST

--- fma/fma.c
+++ fma/fma.c
@@ -24,6 +24,10 @@ void fma_ff(float *A, float *B, float C, float *res)
  *   Mostly glommed from:
  *   https://en.wikipedia.org/wiki/Horner%27s_method
  */
+
+/*
+ *   AVX
+ */
 inline static __m128 _fma_ff_vec(__m128 *veca, __m128 *vecb, float C)
 {
   __m128 vecc = _mm_set1_ps(C);
@@ -68,6 +72,65 @@ void horner_fma_dp(double *x, double *coeffs, unsigned int count, double *res)
 
   res[0] = pres[0];
   res[1] = pres[1];
+
+  return;
+}
+
+
+/*
+ *   AVX2
+ */
+inline static __m256 _fma_ff_vec_avx2(__m256 *veca, __m256 *vecb, float C)
+{
+  __m256 vecc = _mm256_set1_ps(C);
+  return _mm256_fmadd_ps(*veca, *vecb, vecc);
+}
+
+void horner_fma_sp_avx2(float *x, float *coeffs, unsigned int count, float *res)
+{
+  __m256 vec_x = _mm256_set_ps(x[7], x[6], x[5], x[4], x[3], x[2], x[1], x[0]);
+  __m256 vec_res = _mm256_set1_ps(0.0f);
+
+  int idx;
+  for (idx = count-1; idx >= 0; idx--)
+    vec_res = _fma_ff_vec_avx2(&vec_res, &vec_x, coeffs[idx]);
+
+  float *pres = (float*) &vec_res;
+
+  res[0] = pres[0];
+  res[1] = pres[1];
+  res[2] = pres[2];
+  res[3] = pres[3];
+  res[4] = pres[4];
+  res[5] = pres[5];
+  res[6] = pres[6];
+  res[7] = pres[7];
+
+  return;
+}
+
+inline static __m256d _fma_dd_vec_avx2(__m256d *veca, __m256d *vecb, double C)
+{
+  __m256d vecc = _mm256_set1_pd(C);
+  return _mm256_fmadd_pd(*veca, *vecb, vecc);
+}
+
+void horner_fma_dp_avx2(double *x, double *coeffs, unsigned int count,
+                		double *res)
+{
+  __m256d vec_x = _mm256_set_pd(x[3], x[2], x[1], x[0]);
+  __m256d vec_res = _mm256_set1_pd(0.0);
+
+  int idx;
+  for (idx = count-1; idx >= 0; idx--)
+    vec_res = _fma_dd_vec_avx2(&vec_res, &vec_x, coeffs[idx]);
+
+  double *pres = (double*) &vec_res;
+
+  res[0] = pres[0];
+  res[1] = pres[1];
+  res[2] = pres[2];
+  res[3] = pres[3];
 
   return;
 }
--- fma/fma.h
+++ fma/fma.h
@@ -2,3 +2,8 @@ void fma_ff(float *A, float *B, float C, float *res);
 
 void horner_fma_sp(float *x, float *coeffs, unsigned int count, float *res);
 void horner_fma_dp(double *x, double *coeffs, unsigned int count, double *res);
+
+void horner_fma_sp_avx2(float *x, float *coeffs, unsigned int count,
+		                float *res);
+void horner_fma_dp_avx2(double *x, double *coeffs, unsigned int count,
+		                double *res);
